573cd089600db46e9211d30006c4b3dcfcb1e49b
DiegoVerdeSvS <diegoverdesv@gmail.com>
2026-02-23 16:02:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
   $('.title').click(function(){
     $('.container').addClass('open');
   });
-  
-  
+
   $('.close').click(function(){
     $('.container').removeClass('open');
   });
